604703fd426fc10f07322042f37eaecdb1df71df
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-09-09 11:38:46 +0200

    .

--- Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -1,64 +1,36 @@
 package engine;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Quiz {
-    final String JSONLoc = "./Questions.json";
+//    final String JSONLoc = "./Questions.json";
 
     private List<Question> questions = new ArrayList<>();
+    private List<Integer> answers = new ArrayList<>();
 
     public Quiz() {
-//        catch questions from json file
-        JSONParser parser = new JSONParser();
-        try {
-            JSONArray questionsArrayJSON = (JSONArray) parser.parse(new FileReader(JSONLoc));
+        String title = "The Java Logo";
+        String text = "What is depicted on the Java logo?";
+        String[] options = new String[]{"Robot","Tea leaf","Cup of coffee","Bug"};
+        int rightAnswer = 2; // cup of coffee
 
-
-            for (Object o : questionsArrayJSON) {
-                JSONObject questionJSON = (JSONObject) o;
-
-                String title = (String) questionJSON.get("title");
-                String text = (String) questionJSON.get("text");
-                JSONArray optionsArray = (JSONArray) questionJSON.get("options");
-                String[] options = new String[optionsArray.size()];
-
-                for (int i = 0; i < optionsArray.size(); i++) {
-                    options[i] = (String) optionsArray.get(i);
-                }
-
-                Question curQuestion = new Question(title, text, options);
-
-                questions.add(curQuestion);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Question tempQuestion = new Question(title, text, options);
+        answers.add(rightAnswer);
+        questions.add(tempQuestion);
     }
 
     @GetMapping(path = "/api/quiz")
-    public int getQuestion() {
-        return questions.size();
+    public Question getQuestion() {
+        return questions.get(0);
     }
 
-    @GetMapping(path = "/api/quiz/{id}")
-    public Question getQuestion(@PathVariable int id) {
-        return questions.get(id);
+    @PostMapping(path = "/api/quiz")
+    public AnswerRes getQuestion(@RequestParam("answer") int answer) {
+        if (answers.get(0) == answer) return new AnswerRes(true, "Congratulations, you're right!");
+        else return new AnswerRes(false, "Wrong answer! Please, try again.");
     }
 }
