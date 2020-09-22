4c0fe40d6e9d54dc322693be4debdcb84fee5ca5
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-09-23 01:47:50 +0200

    .

--- Web Quiz Engine/task/src/engine/Question.java
+++ Web Quiz Engine/task/src/engine/Question.java
@@ -1,21 +1,47 @@
 package engine;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 public class Question {
     private int id;
-    final private String title;
-    final private String text;
-    final private String[] options;
+
+    @NotNull(message = "Please enter title")
+    @NotEmpty
+    private String title;
+
+    @NotNull(message = "Please enter text")
+    @NotEmpty
+    private String text;
+
+    @NotNull
+    @NotEmpty
+    private String[] options;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    final private int answer;
+//    @NotNull
+//    @NotEmpty
+    private int[] answer;
 
-    public Question(String title, String text, String[] options, int answer) {
+    public Question() {
+    }
+
+    public Question(String title, String text, String[] options, int[] answer) {
         this.title = title;
         this.text = text;
         this.options = options;
         this.answer = answer;
+    }
+
+    public Question(String title, String text, String[] options) {
+        this.title = title;
+        this.text = text;
+        this.options = options;
+        this.answer = new int[0];
     }
 
     public void setId(int id) {
@@ -38,8 +64,28 @@ public class Question {
         return text;
     }
 
-    public boolean ifGuessed(int answer) {
-        return answer == this.answer;
+    public boolean ifGuessed(int[] answer) {
+        if (answer.length != this.answer.length) return false;
+
+        return Objects.equals(answer, this.answer);
+
+//        boolean isCorrect = true;
+
+//        for (int a : this.answer) {
+//            boolean is = false;
+//            for (int aTemp : answer) {
+//                if (aTemp == a) {
+//                    is = true;
+//                    break;
+//                }
+//            }
+//            if (!is) {
+//                isCorrect = false;
+//                break;
+//            }
+//        }
+
+//        return isCorrect;
     }
 }
 
--- Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -1,101 +1,24 @@
 package engine;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class Quiz {
     private List<Question> questions = new ArrayList<>();
 
-    private List<Question> getQuestionsFromFile() {
-        List<Question> questions = new ArrayList<>();
-        JSONParser jsonParser = new JSONParser();
-
-        try {
-            Path fileLoc = ResourceUtils.getFile("Questions.json").toPath();
-            FileReader reader = new FileReader(String.valueOf(fileLoc));
-//            read JSON file
-            JSONArray a = (JSONArray) jsonParser.parse(reader);
-
-            for (Object o : a) {
-                JSONObject questionObj = (JSONObject) o;
-
-                String title = (String) questionObj.get("title");
-
-                String text = (String) questionObj.get("text");
-
-                JSONArray optionsObj = (JSONArray) questionObj.get("options");
-                List<String> optionsList = new ArrayList<>();
-                for (Object option : optionsObj) {
-                    optionsList.add((String) option);
-                }
-                String[] optionsString = new String[optionsObj.size()];
-                for (int i = 0; i < optionsList.size(); i++) {
-                    optionsString[i] = optionsList.get(i);
-                }
-
-
-                int answer = (int) questionObj.get("answer");
-
-                Question question = new Question(title, text, optionsString, answer);
-                questions.add(question);
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return questions;
-    }
-
-    private List<Question> setQuestions() {
-        final List<Question> questions = new ArrayList<>();
-
-        String[] title = new String[]{
-                "The java logo",
-                "The Ultimate Question"
-        };
-        String[] text = new String[]{
-                "What is depicted on the Java logo?",
-                "What is the answer to the Ultimate Question of Life, the Universe and Everything?"
-        };
-        String[][] options = new String[][]{
-                {"Robot","Tea leaf","Cup of coffee","Bug"},
-                {"Everything goes right","42","2+2=4","11011100"}
-        };
-        int[] answer = new int[]{
-                2,
-                1
-        };
-
-        for (int i = 0; i < title.length; i++) {
-            questions.add(new Question(title[i], text[i], options[i], answer[i]));
-        }
-
-        return questions;
-    }
-
     @PostMapping(path = "/api/quizzes")
-    public Question addQuestion(@RequestBody Question question) {
+    public Question addQuestion(@Valid @RequestBody Question question) {
         question.setId(questions.size());
+
+        if (question.getOptions().length <= 2) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid fields!");
 
         questions.add(question);
 
@@ -115,9 +38,19 @@ public class Quiz {
     }
 
     @PostMapping(path = "/api/quizzes/{id}/solve")
-    public AnswerRes solveQuestion(@PathVariable int id, @RequestParam("answer") int answer) {
+    public AnswerRes solveQuestion(@PathVariable int id, /*@RequestParam(value = "answer", required = false)*/@RequestBody int[] answer) {
         if (id >= questions.size() || id < 0) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such quiz");
 
+        if (answer == null) answer = new int[0];
+
         return new AnswerRes(questions.get(id).ifGuessed(answer));
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Incorrect fields!")
+    public HashMap<String, String> handleBadRequest(Exception e) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", "BAD_REQUEST");
+        response.put("error", e.getClass().getSimpleName());
+        return response;
     }
 }
